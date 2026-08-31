a0fab88442211baf88bab111b33636e28443c3db
islow0001 <serbakovs201@gmail.com>
2026-08-31 19:34:46 +0300

    test2

--- custom_components/yandex_media/__init__.py
+++ custom_components/yandex_media/__init__.py
@@ -1,5 +1,26 @@
-DOMAIN = "yandex_media"
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
 
-async def async_setup(hass, config):
-    hass.data[DOMAIN] = {}
+from .const import DOMAIN
+
+PLATFORMS = [Platform.MEDIA_SOURCE]
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up integration from configuration.yaml (legacy)."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up integration from UI."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {}
+    
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload integration."""
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
---
+++ custom_components/yandex_media/const.py
@@ -0,0 +1,1 @@
+DOMAIN = "yandex_media"
